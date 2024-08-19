867793b72fc9065f96011909b7d476a85a0af468
Jose Escribano <jescribanomora@gmail.com>
2024-08-19 13:31:10 +0200

    Process request body

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,32 @@
 const { response } = require("express");
 
 const loginUser = (req, res = response) => {
+    const { email, password } = req.body;
+
     res.json({
         ok: true,
         msg: "login",
+        email,
+        password,
     });
 };
 
 const createNewUser = (req, res = response) => {
-    res.json({
+    const { name, email, password } = req.body;
+
+    if (name.length < 5) {
+        return res.status(400).json({
+            ok: false,
+            msg: "Name must be at least 5 characters long",
+        });
+    }
+
+    res.status(201).json({
         ok: true,
         msg: "register",
+        name,
+        email,
+        password,
     });
 };
 
