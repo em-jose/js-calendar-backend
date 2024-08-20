18a8a7f0f39df8be33ee9ee7846dc3ac47c8535b
Jose Escribano <jescribanomora@gmail.com>
2024-08-20 10:22:12 +0200

    Validate dates on event creation

--- routes/events.js
+++ routes/events.js
@@ -3,6 +3,9 @@
     /api/events
 */
 const { Router } = require("express");
+const { check } = require("express-validator");
+const { isDate } = require("../helpers/isDate");
+const { fieldValidate } = require("../middlewares/field-validator");
 const { jwtValidate } = require("../middlewares/jwt-validator");
 const router = Router();
 const {
@@ -17,7 +20,16 @@ router.use(jwtValidate);
 
 router.get("/", getEvents);
 
-router.post("/", createEvent);
+router.post(
+    "/",
+    [
+        check("title", "Title is required").not().isEmpty(),
+        check("start", "Starting date is required").custom(isDate),
+        check("end", "Ending date is required").custom(isDate),
+        fieldValidate,
+    ],
+    createEvent
+);
 
 router.put("/:id", updateEvent);
 
