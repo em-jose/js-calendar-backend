7350d198e2ac5668a8fcc96418bdec8283cb1621
Jose Escribano <jescribanomora@gmail.com>
2024-08-20 11:45:04 +0200

    Implement getEvents funcionality

--- controllers/events.js
+++ controllers/events.js
@@ -1,10 +1,12 @@
 const { response } = require("express");
 const Event = require("../models/Event");
 
-const getEvents = (req, res = response) => {
+const getEvents = async (req, res = response) => {
+    const events = await Event.find().populate("user", "name");
+
     return res.json({
         ok: true,
-        msg: "getEvents",
+        events: events,
     });
 };
 
