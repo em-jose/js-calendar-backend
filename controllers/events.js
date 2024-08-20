8a93eb14f7bb05cdca45cc0a734548ba470d33d5
Jose Escribano <jescribanomora@gmail.com>
2024-08-20 11:20:01 +0200

    Implement create event funcionality

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const Event = require("../models/Event");
 
 const getEvents = (req, res = response) => {
     return res.json({
@@ -7,11 +8,24 @@ const getEvents = (req, res = response) => {
     });
 };
 
-const createEvent = (req, res = response) => {
-    return res.json({
-        ok: true,
-        msg: "createEvent",
-    });
+const createEvent = async (req, res = response) => {
+    const event = new Event(req.body);
+
+    try {
+        event.user = req.uid;
+        const savedEvent = await event.save();
+
+        return res.json({
+            ok: true,
+            event: savedEvent,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: "Talk with the admin",
+        });
+    }
 };
 
 const updateEvent = (req, res = response) => {
