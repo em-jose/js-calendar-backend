75576cdbcde444fbea4572bb0b7734ceba000f6c
Jose Escribano <jescribanomora@gmail.com>
2024-08-20 07:30:33 +0200

    Validate email on user register

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,14 +13,26 @@ const loginUser = (req, res = response) => {
 };
 
 const createNewUser = async (req, res = response) => {
+    const { email, password } = req.body;
+
     try {
-        const user = new User(req.body);
+        let user = await User.findOne({ email });
+
+        if (user) {
+            res.status(400).json({
+                ok: false,
+                msg: "The email already exists",
+            });
+        }
+
+        user = new User(req.body);
 
         await user.save();
 
         res.status(201).json({
             ok: true,
-            msg: "register",
+            uid: user.id,
+            name: user.name,
         });
     } catch (error) {
         console.log(error);
